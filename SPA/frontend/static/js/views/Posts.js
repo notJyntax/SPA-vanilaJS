43b79402cd189d5087d0ef7f2d9a57375d79faa8
homeTax <iwillrule@outloo.kr>
2020-12-28 14:38:46 +0900

    Add files

---
+++ SPA/frontend/static/js/views/Posts.js
@@ -0,0 +1,17 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("Posts");
+    }
+
+    async getHtml() {
+        return `
+            <h1>Posts</h1>
+            <p>
+                Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
+            </p>
+        `;
+    }
+}
---
+++ SPA/frontend/static/js/views/Settings.js
@@ -0,0 +1,17 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("Settings");
+    }
+
+    async getHtml() {
+        return `
+            <h1>Settings</h1>
+            <p>
+                Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
+            </p>
+        `;
+    }
+}
